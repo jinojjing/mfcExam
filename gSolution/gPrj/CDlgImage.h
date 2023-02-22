4d83f9ec479843b3a651f923f6c06fb5c5275439
jinojjing <118786381+jinojjing@users.noreply.github.com>
2023-02-22 22:29:21 +0900

    우측 이미지에 정보 전달

--- gSolution/gPrj/CDlgImage.h
+++ gSolution/gPrj/CDlgImage.h
@@ -14,6 +14,8 @@ public:
 
 	CImage m_image;
 	CWnd* m_pParent;
+	int m_nDataCount = 0;
+	CPoint m_ptData[100];
 
 // 대화 상자 데이터입니다.
 #ifdef AFX_DESIGN_TIME
@@ -26,5 +28,12 @@ protected:
 	DECLARE_MESSAGE_MAP()
 public:
 	afx_msg void OnBnClickedBtnUpParent();
-	// virtual BOOL OnInitDialog();
+	virtual BOOL OnInitDialog();
+	
+private:
+	 void InitImage();
+	 void drawData(CDC* pDC);
+
+public:
+	afx_msg void OnPaint();
 };
